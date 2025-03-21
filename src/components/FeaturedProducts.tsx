a6dd92c2b99367fcef200694a27121836cf82992
Shashi8066 <shashanksk8077@gmail.com>
2025-03-22 00:01:26 +0530

    Update FeaturedProducts.tsx

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -10,28 +10,28 @@ const featuredProducts = [
     name: 'Premium Shiitake',
     category: 'Culinary',
     price: 24.99,
-    imageSrc: 'https://images.unsplash.com/photo-1543364195-bfe6e4932397?q=80&w=1887&auto=format&fit=crop'
+    imageSrc: 'https://i.pinimg.com/474x/e5/45/6e/e5456e35805b7c7b21a4c72de8899bef.jpg'
   },
   {
     id: 'reishi-organic',
     name: 'Organic Reishi',
     category: 'Medicinal',
     price: 39.99,
-    imageSrc: 'https://images.unsplash.com/photo-1665140666193-9a33fcb06c8f?q=80&w=2070&auto=format&fit=crop'
+    imageSrc: 'https://i.pinimg.com/474x/14/51/78/145178eb0587af49d86920e862976549.jpg'
   },
   {
     id: 'lions-mane',
     name: "Lion's Mane",
     category: 'Gourmet',
     price: 29.99,
-    imageSrc: 'https://images.unsplash.com/photo-1624458497711-dac9afa55a25?q=80&w=2070&auto=format&fit=crop'
+    imageSrc: 'https://i.pinimg.com/474x/60/62/b6/6062b609c438293850e66c09ac1eaea6.jpg'
   },
   {
     id: 'truffle-black',
     name: "Black Truffle",
     category: 'Luxury',
     price: 89.99,
-    imageSrc: 'https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?q=80&w=2070&auto=format&fit=crop'
+    imageSrc: 'https://i.pinimg.com/736x/3e/92/21/3e9221567c8df0c207677046f0a609de.jpg'
   }
 ];
 
@@ -72,3 +72,4 @@ const FeaturedProducts: React.FC = () => {
 };
 
 export default FeaturedProducts;
+
